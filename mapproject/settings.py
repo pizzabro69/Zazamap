430e80509a4d17e72d458c0153af4dd9668fb937
Stephen de Vries <s1218455@student.windesheim.nl>
2025-05-11 16:16:32 +0200

    Update settings for production with STATIC_ROOT and ALLOWED_HOSTS

--- mapproject/settings.py
+++ mapproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1vtk!*6o#u++04oljkcx2artb2oaj380eo*3+!&#7t6m6bhim'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pizzabro.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
